871e2c6c7497382444c09afec16b8d24d2134b37
Founntain <7@founntain.dev>
2023-04-02 11:46:18 +0200

    Added ViewLocator.cs

---
+++ Nein/View/ViewLocator.cs
@@ -0,0 +1,48 @@
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using Avalonia.Layout;
+using Nein.Base;
+using Nein.Extensions;
+using Splat;
+
+namespace Nein.View;
+
+public class ViewLocator : IDataTemplate
+{
+    public bool SupportsRecycling => false;
+
+    public IControl Build(object data)
+    {
+        var name = data.GetType().FullName?.Replace("ViewModel", "View");
+
+        if (name == null)
+            return OnFail("");
+
+        var type = Type.GetType(name);
+
+        if (Locator.Current.GetService(type) is Control serviceView) return serviceView;
+
+        if (type != null && Activator.CreateInstance(type) is Control view) return view;
+
+        return OnFail(name);
+    }
+
+    public bool Match(object data)
+    {
+        return data is BaseViewModel;
+    }
+
+    private IControl OnFail(string name)
+    {
+        var button = new Button
+        {
+            Content = $"Not Found: {name}, please buy at founntain.dev",
+            VerticalAlignment = VerticalAlignment.Center,
+            HorizontalAlignment = HorizontalAlignment.Center
+        };
+
+        button.Click += (_, _) => GeneralExtensions.OpenUrl(@"https://founntain.dev");
+
+        return button;
+    }
+}
